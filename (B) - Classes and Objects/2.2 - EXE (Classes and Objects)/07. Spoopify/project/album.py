427599db67e20f79341dfe6193284434d32bc0e9
cristov_7 <114032977+cristov7@users.noreply.github.com>
2023-03-03 12:09:09 +0200

    Python
    Python

--- (B) - Classes and Objects/2.2 - EXE (Classes and Objects)/07. Spoopify/project/album.py
+++ (B) - Classes and Objects/2.2 - EXE (Classes and Objects)/07. Spoopify/project/album.py
@@ -1,43 +1,52 @@
-from project.song import Song
 from typing import Tuple, List
+from project.song import Song
 
 
 class Album:
-    def __init__(self, name: str, *songs: Tuple[Song]):
+    def __init__(self, name: str, *songs: Tuple[Song]):   # (song_objects)
         self.name = name
-        self.songs: List[Song] = list(*songs)
+        self.songs: List[Song] = list(*songs)   # [song_objects]
         self.published: bool = False
 
     def add_song(self, song: Song) -> str:
         if song.single:   # if song.single == True:
-            return f"Cannot add {song.name}. It's a single"
-        elif self.published:   # if self.published == True:
+            song_name = song.name
+            return f"Cannot add {song_name}. It's a single"
+        elif self.published:   # elif self.published == True:
             return "Cannot add songs. Album is published."
         elif song in self.songs:
             return "Song is already in the album."
-        else:
+        else:   # elif song.single == False and self.published == False and song not in self.songs:
             self.songs.append(song)
-            return f"Song {song.name} has been added to the album {self.name}."
+            song_name = song.name
+            name = self.name
+            return f"Song {song_name} has been added to the album {name}."
 
     def remove_song(self, song_name: str) -> str:
-        try:
-            song = [song_info for song_info in self.songs if song_info.name == song_name][0]
-        except IndexError:
-            return "Song is not in the album."
         if self.published:   # if self.published == True:
             return "Cannot remove songs. Album is published."
-        else:
-            self.songs.remove(song)
-            return f"Removed song {song_name} from album {self.name}."
+        else:   # elif self.published == False:
+            song_object_list = [song_object for song_object in self.songs
+                                if song_object.name == song_name]
+            if song_object_list:   # if len(song_object_list) > 0:
+                song_object = song_object_list[0]
+                self.songs.remove(song_object)
+                album_name = self.name
+                return f"Removed song {song_name} from album {album_name}."
+            else:   # elif len(song_object_list) == 0:
+                return "Song is not in the album."
 
     def publish(self) -> str:
+        name = self.name
         if self.published:   # if self.published == True:
-            return f"Album {self.name} is already published."
+            return f"Album {name} is already published."
         else:   # elif self.published == False:
             self.published = True
-            return f"Album {self.name} has been published."
+            return f"Album {name} has been published."
 
     def details(self) -> str:
-        songs_info = "\n".join([f"== {song_info.get_info()}" for song_info in self.songs])
-        return f"Album {self.name}" \
+        name = self.name
+        songs_info = "\n".join([f"== {song_object.get_info()}"
+                                for song_object in self.songs])
+        return f"Album {name}" \
                f"\n{songs_info}"
--- (B) - Classes and Objects/2.2 - EXE (Classes and Objects)/07. Spoopify/project/band.py
+++ (B) - Classes and Objects/2.2 - EXE (Classes and Objects)/07. Spoopify/project/band.py
@@ -1,31 +1,37 @@
-from project.album import Album
 from typing import List
+from project.album import Album
 
 
 class Band:
     def __init__(self, name: str):
         self.name = name
-        self.albums: List[Album] = []
+        self.albums: List[Album] = []   # [album_objects]
 
     def add_album(self, album: Album) -> str:
+        band_name = self.name
+        album_name = album.name
         if album in self.albums:
-            return f"Band {self.name} already has {album.name} in their library."
-        else:
+            return f"Band {band_name} already has {album_name} in their library."
+        else:   # elif album not in self.albums:
             self.albums.append(album)
-            return f"Band {self.name} has added their newest album {album.name}."
+            return f"Band {band_name} has added their newest album {album_name}."
 
     def remove_album(self, album_name: str) -> str:
-        try:
-            album = [album_info for album_info in self.albums if album_info.name == album_name][0]
-        except IndexError:
+        album_objects_list = [album_object for album_object in self.albums
+                              if album_object.name == album_name]
+        if album_objects_list:   # if len(album_objects_list) > 0:
+            album_object = album_objects_list[0]
+            if album_object.published:   # if album_object.published == True:
+                return "Album has been published. It cannot be removed."
+            else:   # elif album_object.published == False:
+                self.albums.remove(album_object)
+                return f"Album {album_name} has been removed."
+        else:   # elif len(album_objects_list) == 0:
             return f"Album {album_name} is not found."
-        if album.published:   # if album.published == True:
-            return "Album has been published. It cannot be removed."
-        else:
-            self.albums.remove(album)
-            return f"Album {album_name} has been removed."
 
     def details(self) -> str:
-        album_details = "\n".join([album_info.details() for album_info in self.albums])
-        return f"Band {self.name}" \
+        name = self.name
+        album_details = "\n".join([album_object.details()
+                                   for album_object in self.albums])
+        return f"Band {name}" \
                f"\n{album_details}"
