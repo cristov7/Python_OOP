edf27d26a3d6d069b52a4dcdd4aa1f9014cfb5a1
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-03-03 06:43:26 +0200

    Python
    Python

--- (B) - Classes and Objects/2.1 - LAB (Classes and Objects)/05. Smartphone.py
+++ (B) - Classes and Objects/2.1 - LAB (Classes and Objects)/05. Smartphone.py
@@ -1,14 +1,17 @@
+from typing import List
+
+
 class Smartphone:
     def __init__(self, memory: int):
         self.memory = memory
-        self.apps = []
-        self.is_on = False
+        self.apps: List[str] = []
+        self.is_on: bool = False
 
     def power(self) -> None:
-        if self.is_on:   # if self.is_on == True:
-            self.is_on = False
-        else:   # elif self.is_on == False:
+        if not self.is_on:   # if self.is_on == False:
             self.is_on = True
+        else:   # elif self.is_on == True:
+            self.is_on = False
 
     def install(self, app: str, app_memory: int) -> str:
         if self.is_on:  # if self.is_on == True:
@@ -34,3 +37,34 @@ class Smartphone:
 # print(smartphone.install("Messenger", 20))
 # print(smartphone.install("Instagram", 40))
 # print(smartphone.status())
+
+
+# from typing import List
+# 
+# 
+# class Smartphone:
+#     def __init__(self, memory: int):
+#         self.memory = memory
+#         self.apps: List[str] = []
+#         self.is_on: bool = False
+# 
+#     def power(self) -> None:
+#         if not self.is_on:   # if self.is_on == False:
+#             self.is_on = True
+#         else:   # elif self.is_on == True:
+#             self.is_on = False
+# 
+#     def install(self, app: str, app_memory: int) -> str:
+#         if self.memory >= app_memory and self.is_on:
+#             self.apps.append(app)
+#             self.memory -= app_memory
+#             return f"Installing {app}"
+#         elif self.memory >= app_memory and not self.is_on:
+#             return f"Turn on your phone to install {app}"
+#         else:
+#             return f"Not enough memory to install {app}"
+# 
+#     def status(self) -> str:
+#         total_apps_count = len(self.apps)
+#         memory_left = self.memory
+#         return f"Total apps: {total_apps_count}. Memory left: {memory_left}"
