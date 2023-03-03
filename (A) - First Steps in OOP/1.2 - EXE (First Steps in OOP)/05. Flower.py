fd9c4ac9db01c513b9fc5f2c9d0f1dcc631910c4
Kristian Hristov <114032977+cristov7@users.noreply.github.com>
2023-03-03 04:19:24 +0200

    Python
    Python

--- (A) - First Steps in OOP/1.2 - EXE (First Steps in OOP)/05. Flower.py
+++ (A) - First Steps in OOP/1.2 - EXE (First Steps in OOP)/05. Flower.py
@@ -2,7 +2,7 @@ class Flower:
     def __init__(self, name: str, water_requirements: int):
         self.name = name
         self.water_requirements = water_requirements
-        self.is_happy = False
+        self.is_happy: bool = False
 
     def water(self, quantity: int) -> None:
         if quantity >= self.water_requirements:
