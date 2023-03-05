d6e6e0df4797e7eee638bd0f22f692c9b747cf3e
cristov_7 <114032977+cristov7@users.noreply.github.com>
2023-03-05 17:50:21 +0200

    Python
    Python

--- (C) - Inheritance/3.2 - EXE (Inheritance)/05. Shop/project/product_repository.py
+++ (C) - Inheritance/3.2 - EXE (Inheritance)/05. Shop/project/product_repository.py
@@ -9,12 +9,12 @@ class ProductRepository:
     def add(self, product: Product) -> None:
         self.products.append(product)
 
-    def find(self, product_name: str) -> [object, None]:
-        product_list = [product_object for product_object in self.products
-                        if product_object.name == product_name]
-        if product_list:   # if len(product_list) > 0:
-            product = product_list[0]
-            return product
+    def find(self, product_name: str) -> [str, None]:
+        product_objects_list = [product_object for product_object in self.products
+                                if product_object.name == product_name]
+        if product_objects_list:   # if len(product_objects_list) > 0:
+            product_object = product_objects_list[0]
+            return product_object   # product_object = product_name
 
     def remove(self, product_name: str) -> None:
         product = self.find(product_name)
@@ -24,7 +24,7 @@ class ProductRepository:
     def __repr__(self) -> str:
         products_info = []
         for product_object in self.products:
-            product_name = product_object
+            product_name = product_object   # product_object = product_object.name
             quantity = product_object.quantity
             product_info = f"{product_name}: {quantity}"
             products_info.append(product_info)
