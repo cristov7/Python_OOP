275f6d11c8d457670574e68490992b167a83b8d7
cristov_7 <114032977+cristov7@users.noreply.github.com>
2023-03-09 10:02:17 +0200

    Python
    Python

--- (D) - Encapsulation/4.2 - EXE (Encapsulation)/04. Restaurant/project/beverage/coffee.py
+++ (D) - Encapsulation/4.2 - EXE (Encapsulation)/04. Restaurant/project/beverage/coffee.py
@@ -2,8 +2,8 @@ from project.beverage.hot_beverage import HotBeverage
 
 
 class Coffee(HotBeverage):
-    MILLILITERS: int = 50
     PRICE: float = 3.50
+    MILLILITERS: int = 50
 
     def __init__(self, name: str, caffeine: float):
         super().__init__(name, Coffee.PRICE, Coffee.MILLILITERS)
